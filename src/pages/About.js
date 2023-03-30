9f35f3942924feb8d6bd98b8724a2d49cad3442a
abey <bruckabey@gmail.com>
2023-03-31 08:56:03 +1030

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum culpa, ullam quis dolore iure cumque voluptas, provident ad aliquid delectus possimus fugiat aperiam itaque facere laudantium laborum voluptatibus excepturi eos deleniti! Nemo debitis harum nostrum eius ab at culpa quis voluptas, velit maxime, atque dicta fugiat soluta? Excepturi, officia!</p>
+    </section>
+  )
+}
+
+export default About
